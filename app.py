fa5df415d4cf0d5deb26b6d318664e07e6adb130
Vinisha <151351411+VShaaa@users.noreply.github.com>
2025-06-24 17:49:44 +0530

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from sentiment import analyze_sentiment, get_word_lists
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)  # Allow requests from frontend
+app = Flask(__name__, template_folder='templates', static_folder='static')
+CORS(app)
 
-# Sentiment Analysis Route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
 @app.route('/analyze', methods=['POST'])
 def analyze():
     data = request.get_json()
@@ -13,10 +20,10 @@ def analyze():
     sentiment, explanation = analyze_sentiment(review)
     return jsonify({'sentiment': sentiment, 'explanation': explanation})
 
-# Word Lists Route (for dropdown in frontend)
 @app.route('/wordlists', methods=['GET'])
 def wordlists():
     return jsonify(get_word_lists())
 
 if __name__ == '__main__':
     app.run(debug=True)
+
